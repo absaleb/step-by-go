8fbfb0f1d2a2175e6d1a5c1e634e5dba3301f4a5
Alexey Samokhin <salebgit@gmail.com>
2018-11-18 00:24:26 +0300

    init

---
+++ awesomeProject/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main()  {
+	dec()
+}
+
+
+func dec(){
+	var strArr []string
+	er := iter(&strArr)
+	if er != nil {
+		fmt.Println(er)
+	} else {
+		fmt.Println("nil error")
+	}
+	fmt.Println(strArr)
+
+	count := 199
+	limit := 100
+	pageCount := count/limit
+
+	fmt.Println(pageCount)
+}
+
+func iter(str *[]string) error{
+	if len(*str) == 20{
+		return errors.New("limit exceed")
+	}
+
+	*str = append(*str, "q ")
+
+	fmt.Println(*str)
+
+	er := iter(str)
+
+	return er
+}
